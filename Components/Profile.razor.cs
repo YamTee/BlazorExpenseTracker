87b5c08ef9945193030707bcf31417df7fd233e2
YamTee <thalha.moha@gmail.com>
2024-08-01 12:17:17 +0530

    Profile Popover Changes

---
+++ Components/Profile.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorExpenseTracker.Components;
+
+public partial class Profile
+{
+    [Parameter]
+    public string? Name { get; set; }
+
+    [Parameter]
+    public string? Initials { get; set; }
+
+    [Parameter]
+    public string? Email { get; set; }
+
+    [Inject]
+    private ILocalStorageService LocalStorage { get; set; } = default!;
+
+    [Inject]
+    private NavigationManager Navigation { get; set; } = default!;
+
+    private async Task LogOutSystem()
+    {
+        await LocalStorage.RemoveItemAsync(LocalStorageKey.ApiKey.GetDescription()!);
+
+        Navigation.NavigateTo("login");
+    }
+
+    private void NavigateToSettings()
+    {
+        Navigation.NavigateTo("settings");
+    }
+}
